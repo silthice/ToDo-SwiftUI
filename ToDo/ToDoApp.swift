daa4c38df1b7b5daaef018c3b1c983fec9bf4e73
silthice <ryonlim@hotmail.com>
2023-01-19 12:55:04 +0800

    -Initial setup

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Giap on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
